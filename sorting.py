0536d50e79c0d17f91985d9dd518f922dd064da3
Alzbeta Krasenska <267020@vutbr.cz>
2026-04-23 15:41:45 +0200

    graf

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -21,6 +22,8 @@ def selection_sort(nums):
         num_copy[min], num_copy[i] = num_copy[i], num_copy[min]
     return num_copy
 
+plt.ion()
+plt.show()
 def bubble_sort(nums):
     num_copy = nums.copy()
     sorted = 0
@@ -28,8 +31,19 @@ def bubble_sort(nums):
         for i in range(1, len(num_copy)):
             if num_copy[i-1] > num_copy[i]:
                 num_copy[i-1], num_copy[i] = num_copy[i], num_copy[i-1]
+            index_highlight1 = i - 1
+            index_highlight2 = i
+            colors = ["steelblue"] * len(num_copy)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(num_copy)), num_copy, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
 
         sorted += 1
+    plt.ioff()
+    plt.show()
     return num_copy
 
 print(bubble_sort(nums))
