f3641a1eb21f2c16514cd1aa6e8f49d40bf007aa
Alzbeta Krasenska <267020@vutbr.cz>
2026-04-23 15:25:50 +0200

    bubble

--- sorting.py
+++ sorting.py
@@ -11,15 +11,25 @@ if __name__ == "__main__":
     print(nums)
 
 
-
 def selection_sort(nums):
-    for i in range(len(nums)):
+    num_copy = nums.copy()
+    for i in range(len(num_copy)):
         min = i
-        for j in range(i+1, len(nums)):
-            if nums[j] < nums[min]:
+        for j in range(i+1, len(num_copy)):
+            if num_copy[j] < num_copy[min]:
                 min = j
-        nums[min], nums[i] = nums[i], nums[min]
-    return nums
+        num_copy[min], num_copy[i] = num_copy[i], num_copy[min]
+    return num_copy
 
-print(selection_sort(nums))
+def bubble_sort(nums):
+    num_copy = nums.copy()
+    sorted = 0
+    while sorted <= len(num_copy):
+        for i in range(1, len(num_copy)):
+            if num_copy[i-1] > num_copy[i]:
+                num_copy[i-1], num_copy[i] = num_copy[i], num_copy[i-1]
 
+        sorted += 1
+    return num_copy
+
+print(bubble_sort(nums))
